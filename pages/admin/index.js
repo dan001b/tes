6729ba598d24dee02b2135e7253be0080c60fd81
dan001b <mustafa.waleed.alqaisy@kreftforeningen.no>
2025-05-05 14:48:23 +0200

    feat: add admin fetch

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
 export default function Admin() {
+  useEffect(async () => {
+    console.log('test');
+    const gql = `
+    query getById($id: ID!) {
+      location_by_pk(id: $id) {
+        name
+        exp
+        token
+      }
+    }
+  `;
+    const response = await fetch('/data-api/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: gql, variables: { id: 'test' } }),
+    });
+    const { data } = await response.json();
+    console.log(data.location_by_pk);
+  }, []);
   return (
     <main>
       <h1>admin</h1>
